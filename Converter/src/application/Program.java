6b180f1ef196059a93646e68eb07d00cbee39ae3
CarlosSergio <carlossergio2401@outlook.com>
2020-06-12 23:27:43 -0300

    Retirando apenas os dados das tuplas

--- Converter/src/application/Program.java
+++ Converter/src/application/Program.java
@@ -5,8 +5,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-import entities.WorkingFile;
-
 /*
  * Esté programa irá ler um arquivo .SQL e converter para .CSV
  * Caminhos dos arquivos SQL 
@@ -18,19 +16,24 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		String filePath = scan.nextLine();		//entrada com o caminho do arquivo
+		//String filePath = scan.nextLine();		//entrada com o caminho do arquivo
+		String filePath = "/home/csergio/Documentos/Banco_de_Dados/smallRelationsInsertFile.sql"; 
 		
 		try (BufferedReader sqlFile = new BufferedReader(new FileReader(filePath))){
 			
-			String lineRaed = sqlFile.readLine();//lê uma linha do arquivo SQL
+			String lineRaed = sqlFile.readLine();	
 			
-			while (lineRaed != null ) {
-				System.out.println(lineRaed);//printa a linha
-				String[] info = lineRaed.split(" ");
-				WorkingFile.printList(info);
-				lineRaed = sqlFile.readLine();
+			while (lineRaed != null ){
+				
+				int begin = lineRaed.indexOf("("); //retornando a posição desse caracter
+				int end = lineRaed.indexOf(")");   //retornando a posição - 1 desse caracter
+				
+				String subLineRead = lineRaed.substring(begin + 1, end);
+				lineRaed  = sqlFile.readLine();//lê uma linha do arquivo SQL
+				
+				System.out.println(subLineRead);
+										
 			}
-			
 			
 		} catch (IOException e) {
 			e.printStackTrace();		//mostar o stack do erro 	
